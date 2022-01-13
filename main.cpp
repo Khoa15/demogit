9a58a62d5c254b90be857be698b6d9a727fde792
pc <pc@DESKTOP-Q0BLKT1>
2022-01-13 21:10:19 +0700

    #define _CRT_SECURE_NO_WARNINGS #include <stdio.h> #include<string.h> #include<conio.h>
    struct date
    {
        int ngay;
        int thang;
        int nam;
    };
    struct SinhVien
    {
        int MSSV;
        char hoten[300];
        char gioitinh[50];
        date ngaysinh;
        char malop[50];
    };
    typedef SinhVien SV;
    void NhapDSSV(SV DS[], int& n);
    void inSV(SV sv);
    SV NhapSV();
    void xoaenter(char x[]);
    void XuatDSSV(SV DS[], int n);
    int TimSVtheoten(SV DS[], int n, char ten[]);
    int XoaSVtheoMSSV(SV DS[], int& n, int mssv);
    
    int main()
    {
    
        SV DS[50];
        int n,mssv;
        int luachon;
            do
            {
                printf("\n--MENU--");
                printf("\n1-nhap va xuat danh sach sinh vien");
                printf("\n2-tim danh sach sinh vien");
                printf("\n3-xoa danh sach sinh vien");
                printf("\n0-thoat chuong trinh");
                printf("\nmoi nhap lua chon cua ban: ");
                scanf("%d", &luachon);
                switch (luachon)
                {
                case 1:
    
                    NhapDSSV(DS, n);
                    XuatDSSV(DS, n);
    
                    break;
                case 2:
                    char tensv[20];
                    printf("nhap ten can tim: ");
                    scanf("%s", &tensv);
                    xoaenter(tensv);
                    printf("ket qua tim kiem ten SV: %d", TimSVtheoten(DS, n, tensv));
                    break;
                case 3:
                    int mssv;
                    printf("nhap mssv: ");
                    scanf("%d", &mssv);
                    printf("\ndanh sach SV sau khi xoa: ");
                    XoaSVtheoMSSV(DS, n, mssv);
                    XuatDSSV(DS, n);
                    break;
                }
            } while (n != 0);
        return 0;
    }
    
    void xoaenter(char x[])
    {
        size_t len = strlen(x);
        if (x[len - 1] == '\n')
        {
            x[len - 1] = '\0';
        }
    }
    SV NhapSV()
    {
        SV sv;
        printf("\nnhap MSSV: ");
        scanf("%d", &sv.MSSV);
        printf("nhap hoten: ");
        do {
            gets_s(sv.hoten);
        } while (strlen(sv.hoten) == 0);// tam thoi
        //xoaenter(sv.hoten);
        printf("nhap gioi tinh: ");
        gets_s(sv.gioitinh);
        //xoaenter(sv.gioitinh);
        printf("nhap ngay sinh: ");
        scanf("%d%d%d", &sv.ngaysinh.ngay, &sv.ngaysinh.thang, &sv.ngaysinh.nam);
        printf("nhap ma lop: ");
        scanf("%s", sv.malop);
        //fflush(stdin);
    
        return sv;
    }
    
    void inSV(SV sv)
    {
        printf("%d\t%-10s\t%-10s\t%-10d/%d/%d\t%-10s\n", sv.MSSV, sv.hoten, sv.gioitinh, sv.ngaysinh.ngay
            , sv.ngaysinh.thang, sv.ngaysinh.nam, sv.malop);
    }
    
    void NhapDSSV(SV DS[], int& n)
    {
        do {
            printf("nhap n: ");
            scanf("%d", &n);
        } while (n <= 0);
        for (int i = 1; i <= n; i++)
        {
            printf("nhap vao sinh vien thu %d: ", i);
            DS[i] = NhapSV();
        }
    }
    
    void XuatDSSV(SV DS[], int n)
    {
        printf("\n%s\t -%-10s\t %-10s\t %-10s \t %-10s\n", "MSSV", "hoten", "gioitinh", "ngaysinh", "malop");
        for (int i = 1; i <= n; i++)
        {
            /*printf("nhap vao sinh vien thu %d: ", i);
            printf("\nMSSV\tHo ten\tGioi tinh\tNgaysinh\tMalop");
            printf("\n");*/
            inSV(DS[i]);
            //printf("\n");
        }
    }
    
    int TimSVtheoten(SV DS[], int n, char ten[])
    {
        for (int i = 1; i <= n; i++)
        {
            if (strstr(DS[i].hoten, ten))
                return 1;
        }
        return 0;
    }
    
    int XoaSVtheoMSSV(SV DS[], int& n, int mssv)
    {
        for (int i = 1; i <= n; i++)
        {
    
            if (DS[i].MSSV == mssv)
            {
                for (int j = i; j <= n; j++)
                {
                    DS[i] = DS[i + 1];
                }
                n--;
            }
        }
        return 0;
    }

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
-#include <stdio.h>
-// Loc
-int sum(int a, int b)
+#define _CRT_SECURE_NO_WARNINGS
+#include"stdio.h"
+#include"string.h"
+
+
+struct date
 {
-	return a + b;
+    int ngay;
+    int thang;
+    int nam;
+};
+struct SinhVien
+{
+    int MSSV;
+    char hoten[30];
+    char gioiTinh[5];
+    date ngaysinh;
+    char malop[20];
+};
+typedef SinhVien SV;
+
+void NhapSV(SV& sv)
+{
+    printf("nhap MSSV: ");
+    scanf("%d", sv.MSSV);
+    printf("nhap hoten: ");
+    scanf("%d", sv.hoten);
+    printf("nhap gioi tinh: ");
+    scanf("%d", sv.gioitinh);
+    printf("nhap ngay sinh: ");
+    scanf("%d%d%d", sv.ngaysinh.ngay,sv.ngaysinh.thang,sv.ngaysinh.nam);
+    printf("nhap ma lop: ");
+    scanf("%d", sv.malop);
 }
-int main() {
-	
-	
-	return 0;
+
+
+void inSV(SV sv)
+{
+    printf("&D\t%s\t%s\t%d/%d/%d\t%s",sv.MSSV,sv.hoten,sv.gioiTinh,sv.ngaysinh.ngay,sv.ngaysinh.thang,sv.ngaysinh.nam,sv.malop)
+}
+int main()
+{
+    SV sv1;
+    NhapSV(sv1);
+    inSV(sv1);
 }
